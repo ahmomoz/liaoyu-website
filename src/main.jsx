fc6ad133fa90365ee73963414af41b95503ea781
ahmomoz <x5426854268@gmail.com>
2025-02-15 13:46:15 +0800

    fix: 補上 HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
@@ -16,7 +17,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <HelmetProvider>
-        <App />
+        <HashRouter>
+          <App />
+        </HashRouter>
       </HelmetProvider>
     </Provider>
   </StrictMode>
