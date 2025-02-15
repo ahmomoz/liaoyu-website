5bff489de76c73dcfd0f98731cef95ad02412e11
ahmomoz <x5426854268@gmail.com>
2025-02-15 13:58:49 +0800

    fix: 手動配置 meta 重定向

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/liaoyu-website/', 
+  base: process.env.NODE_ENV === 'production' ? '/liaoyu-website/' : '/', 
   plugins: [react()],
 })
