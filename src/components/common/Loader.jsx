c73597939909078be7d67cf1630cb882b93668e1
ahmomoz <x5426854268@gmail.com>
2025-02-15 14:13:21 +0800

    fix: 修改 loading 圖片路徑

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -1,7 +1,10 @@
 export default function Loader() {
   return (
     <div className="loader-wrapper">
-      <img src="/images/image/loading-img2.gif" alt="loading-image" />
+      <img
+        src="https://raw.githubusercontent.com/ahmomoz/liaoyu-website/refs/heads/main/public/images/image/loading-img2.gif"
+        alt="loading-image"
+      />
     </div>
   );
 }
